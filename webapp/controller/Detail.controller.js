a7f659ea5d51eb1bdc1162399268095962af411f
Schopfer Jonathan <Jonathan.Schopfer@novo-bc.ch>
2023-11-16 14:15:13 +0000

    layout angepasst 2 tag

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -34,10 +34,42 @@ sap.ui.define([
                         }
                     }
                 });
+                var oViewRoom = this.byId("idView") ; 
+                // oViewRoom.bindElement({
+                //     path: "/Presentations('" + oArgs.presentationId + "')",
+
+                    
+                //     parameters: {
+                //       $expand: "presentations($expand=room)"
+                //     },
+
+
+
+                //     events: {
+                //         dataRequested: function () {
+                //             oViewRoom.setBusy(true);
+                //         },
+                //         dataReceived: function () {
+                //             oViewRoom.setBusy(false);
+                //         }
+                //     }
+                // });
+
+
+
+
+
+
+
+
             },
             handleNavButtonPress: function (evt) {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("home");
+            },
+
+            onPressView: function(evt){
+                
             },
 
             handleOrder: function (evt) {
